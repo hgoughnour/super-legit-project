cd12cf61ed877d92477f814db47ac1b1cfcbfbb1
egoughnour <e.goughnour@gmail.com>
2016-04-02 11:58:55 -0500

    adding initial changes for testing
    trying to set up travis

---
+++ src/MediaUtility/TestCases/AdderTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using MediaLibrary;
+
+namespace TestCases
+{
+    [TestFixture]
+    public class AdderTests
+    {
+        [Test]
+        public void TestSum()
+        {
+            var a = 3;
+            var b = 7;
+
+            var expected = 10;
+
+            var adder = new Adder();
+
+            var actual = adder.Sum(a, b);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ src/MediaUtility/Utility/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediaLibrary;
+
+namespace Utility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var adder = new Adder();
+            var a = 2;
+            var b = 4;
+            Console.WriteLine($"{a} + {b} = {adder.Sum(a, b)}");
+
+        }
+    }
+}
